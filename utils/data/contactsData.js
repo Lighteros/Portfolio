c1f4497d8b13625cddb31f3109521b461d2a4d28
Lighteros <Lightningdev722@gmail.com>
2024-05-05 07:12:44 -0400

    first commit

---
+++ utils/data/contactsData.js
@@ -0,0 +1,11 @@
+export const contactsData = {
+    email: 'webm07871@gmail.com',
+    phone: '+8801608797655',
+    address: 'Sendai, Miyagi, Japan',
+    github: 'https://github.com/Lighteros',
+    facebook: 'https://www.facebook.com',
+    linkedIn: 'https://www.linkedin.com/in',
+    twitter: 'https://x.com/leodev722',
+    stackOverflow: 'https://stackoverflow.com',
+    devUsername: "said7388"
+}
---
+++ utils/data/personal-data.js
@@ -0,0 +1,17 @@
+export const personalData = {
+  name: "Kaede",
+  profile: '/profile.png',
+  designation: "Software Developer",
+  description: "A dedicated full stack developer specializing in frontend design and development. With a passion for creating captivating user experiences, Bring a strong skill set in HTML, CSS, and JavaScript, along with expertise in frontend frameworks like React or Angular. Focuses in crafting visually stunning and seamless user interfaces that not only engage users but also provide intuitive navigation. Smart contract specialist building secure crypto products on EVM & Solana chains using Solidity & Rust.",
+  email: 'webm07871@gmail.com',
+  phone: '+8801608797655',
+  address: 'Sendai, Miyagi, Japan',
+  github: 'https://github.com/Lighteros',
+  facebook: 'https://www.facebook.com',
+  linkedIn: '#',
+  twitter: 'https://twitter.com/leodev722',
+  stackOverflow: 'https://stackoverflow.com',
+  leetcode: "https://leetcode.com/said3812/",
+  devUsername: "said7388",
+  resume: "https://drive.google.com/file/d/1eyutpKFFhJ9X-qpQGKhUNnVRkB5Wer00/view?usp=sharing"
+}
