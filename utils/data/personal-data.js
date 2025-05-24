4d9ed20acd2702e22cfe9baf1f2d5becf7b48852
Lighteros <lightningdev722@gmail.com>
2025-05-24 12:33:41 -0400

    fix

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -1,6 +1,7 @@
 export const personalData = {
   name: "Kaede",
   profile: '/profile.png',
+  phone: '+81 9070207379',
   designation: "Software Developer",
   description: "Full Stack Developer with 8+ years of experience in designing, developing, and maintaining web applications. Proficient in front-end and back-end development, with expertise in programming design, development, and implementation using a range of technologies and programming languages. Collaborative team player with strong problem-solving skills and a proven track record of developing highly functional web applications. Certified in Full-Stack Web Development with React and Front End Web Development. Passionate about staying up-to-date with the latest technologies and trends in the industry.",
   email: 'webm07871@gmail.com',
